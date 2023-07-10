e425bfebb9e22e7e1438e23c689c89fb4bb13d1b
Adam <adamgjohnson92@gmail.com>
2023-07-10 12:41:22 -0700

    created functioning schema.sql file that generates tables for departments, roles, and employees.

--- db/schema.sql
+++ db/schema.sql
@@ -5,21 +5,32 @@ USE management_db;
 
 CREATE TABLE departments (
   id INT NOT NULL PRIMARY KEY,
-  department_name VARCHAR(30) NOT NULL
+  name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles (
   id INT NOT NULL PRIMARY KEY,
-  role_name VARCHAR(30) NOT NULL,
-)
-
-CREATE TABLE employees (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL
+  job_title VARCHAR(30) NOT NULL,
+  department_id INT,
+  FOREIGN KEY (department_id)
+  REFERENCES departments(id)
+  ON DELETE SET NULL,
+  salary INT NOT NULL
 );
 
- FOREIGN KEY (movie_id)
-  REFERENCES movies(id)
-  ON DELETE SET NULL;
+CREATE TABLE employees (
+  id INT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    job_title INT,
+    department_id INT,
+    salary INT NOT NULL,
+    manager VARCHAR(30) NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES departments(id)
+    ON DELETE SET NULL,
+    FOREIGN KEY (job_title)
+    REFERENCES roles(id)
+    ON DELETE SET NULL
+);
+
---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO movies (id, name)
+VALUES ("Jurassic Park"),
+       ("Mad Max"),
+       ("Young Frankenstein"),
+       ("Tarzan");
+
+INSERT INTO reviews (id, review, movie_id)
+VALUES ("Cool dinosaurs!", 1),
+       ("Love dinosaurs. Coool!1", 1),
+       ("Fast cars go boom!", 2),
+       ("So funny!", 3),
+       ("Puttin' on the Ritz!", 3),
+       ("Gorillas.", 4 ),
+       ("Jungle is dope.", 4);
