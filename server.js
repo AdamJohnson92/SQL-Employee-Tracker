f5af34db918db6fea76a8063ae3d982a66f57edb
Adam <adamgjohnson92@gmail.com>
2023-07-10 17:38:47 -0700

    Fleshed out inquirer prompts to navigate to separate question arrays depending on which option was selected in the opening menu.

--- server.js
+++ server.js
@@ -14,12 +14,13 @@ const db = mysql.createConnection(
     host: 'localhost',
     user: 'root',
     password: 'MakiTheMenace1734!',
-    database: 'courses_db'
+    database: 'management_db'
   },
-  console.log(`Connected to the courses_db database.`)
+  console.log(`Connected to the management_db database.`)
 );
 
-const menu = 
+function init() {
+    const menu = 
     {
         type: 'list',
         name: 'menu',
@@ -28,13 +29,21 @@ const menu =
     }
 
 
-function init() {inquirer
+    inquirer
     .prompt(menu) 
     .then(function(data) {
-            if (menu.choices === 'Exit') {
-                console.log(data)
+            if (data.menu === 'Exit') {
                 console.log(data)
                 console.log('\n Good Bye.')
+            } else if (data.menu === 'Add a department'){
+                console.log(data)
+                addDepartmentHandler()
+            } else if (data.menu === 'Add a role'){
+                console.log(data)
+                addRoleHandler()
+            } else if (data.menu === 'Add an employee'){
+                console.log(data)
+                addEmployeeHandler()
             } else {
                 console.log(data)
                 init()
@@ -65,5 +74,112 @@ function init() {inquirer
 //   app.listen(port, () => {
 //     console.log(`Server running on port ${port}`);
 //   });  
-  
+
+function addDepartmentHandler(){
+    const addDepartmentQuestions = [
+        {
+            type: 'input',
+            name: 'departmentID',
+            message: 'Please enter a new 3-digit ID number for the new department'
+        },
+
+        {
+            type: 'input',
+            name: 'departmentName',
+            message: 'Please enter the name of the new department'
+        }
+    ]
+
+    inquirer
+    .prompt(addDepartmentQuestions) 
+    .then(function(data) {
+        console.log(data)
+        init()
+    })
+};
+
+function addRoleHandler(){
+    const addRoleQuestions = [
+        {
+            type: 'input',
+            name: 'roleID',
+            message: 'Please enter a new 3-digit ID number for the new role'
+        },
+
+        {
+            type: 'input',
+            name: 'jobTitle',
+            message: 'Please enter the new job title'
+        },
+
+        {
+            type: 'input', 
+            name: 'roleDepID',
+            message: 'Please enter the ID of the department to which this new role will belong'
+        },
+
+        {
+            type: 'input',
+            name: 'roleSalary',
+            message: 'Please enter the salary for this new position. Please only use whole numbers, and do NOT include dollar signs or commas.'
+        }
+
+    ]
+
+    inquirer
+    .prompt(addRoleQuestions) 
+    .then(function(data) {
+        console.log(data)
+        init()
+    })
+};
+
+
+function addEmployeeHandler() {
+    const addEmployeeQuestions = [
+        {
+            type: 'input',
+            name: 'employeeID',
+            message: 'Please enter a new 3-digit ID number for the new employee'
+        },
+
+        {
+            type: 'input',
+            name: 'firstName',
+            message: "Please enter the new employee's first name"
+        },
+
+        {
+            type: 'input',
+            name: 'lastName',
+            message: "Please enter the new employee's last name"
+        },
+
+        {
+            type: 'input', 
+            name: 'employeeDepID',
+            message: 'Please enter the ID of the department to which this new employee will belong'
+        },
+
+        {
+            type: 'input', 
+            name: 'employeeRoleID',
+            message: 'Please enter the ID of the job title of the position this new employee is filling'
+        },
+
+        {
+            type: 'input',
+            name: 'employeeSalary',
+            message: "Please enter the salary for this new employee's position. Please only use whole numbers, and do NOT include dollar signs or commas."
+        }
+    ]
+
+    inquirer
+    .prompt(addEmployeeQuestions) 
+    .then(function(data) {
+        console.log(data)
+        init()
+    })
+};
+
 init();
