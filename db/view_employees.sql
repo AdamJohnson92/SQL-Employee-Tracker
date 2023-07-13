fc01092037995bc45c7ac9677dee3153460ace45
Adam <adamgjohnson92@gmail.com>
2023-07-12 18:32:29 -0700

    Corrected self join for manager ID to display as manager's first and last name

--- db/view_employees.sql
+++ db/view_employees.sql
@@ -2,5 +2,5 @@
 
 -- SELECT employees.id, employees.first_name, employees.last_name, roles.job_title, roles.salary, departments.department_name FROM employees LEFT JOIN roles ON employees.job_title_id = roles.id LEFT JOIN departments ON roles.department_id = departments.id;
 
-SELECT employees.id, employees.first_name, employees.last_name, roles.job_title, roles.salary, departments.department_name, employees.manager_id FROM employees LEFT JOIN roles ON employees.job_title_id = roles.id LEFT JOIN departments ON roles.department_id = departments.id
-JOIN employees ON employees.last_name = employees.id;
+SELECT employees.id, employees.first_name, employees.last_name, roles.job_title, roles.salary, departments.department_name, employees.manager_id, CONCAT(manager.first_name,' ', manager.last_name) AS manager FROM employees LEFT JOIN roles ON employees.job_title_id = roles.id LEFT JOIN departments ON roles.department_id = departments.id
+LEFT JOIN employees manager ON manager.id = employees.manager_id;
--- server.js
+++ server.js
@@ -57,7 +57,7 @@ function init() {
                     init();
                 });
             } else if (data.menu === 'View all employees') {
-                db.query('SELECT employees.id, employees.first_name, employees.last_name, roles.job_title, roles.salary, departments.department_name, employees.manager_id FROM employees LEFT JOIN roles ON employees.job_title_id = roles.id LEFT JOIN departments ON roles.department_id = departments.id;', function (err, results) {
+                db.query('SELECT employees.id, employees.first_name, employees.last_name, roles.job_title, roles.salary, departments.department_name, CONCAT(manager.first_name," ", manager.last_name) AS manager FROM employees LEFT JOIN roles ON employees.job_title_id = roles.id LEFT JOIN departments ON roles.department_id = departments.id LEFT JOIN employees manager ON manager.id = employees.manager_id;', function (err, results) {
                     console.table(results);
                     console.log('\n');
                     init();
